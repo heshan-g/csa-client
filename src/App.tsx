91ffbace45f7f4756527697ec0b796d8f24d163f
Heshan <heshan.ua@gmail.com>
2023-07-17 18:52:16 +0530

    Add MUI and the login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
+import { useState } from 'react'
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  const [currentPage, setCurrentPage] = useState('dashboard');
+
+  const displayPage = (page: string) => {
+    if (!isAuth) {
+      return <Login />;
+    } 
+
+    return (
+      <>
+        <div>Header</div>
+        {page === 'dashboard' && <Dashboard />}
+      </>
+    );
+  }
+
   return (
     <>
-      <div>CSA</div>
+      {displayPage(currentPage)}
     </>
   )
 }
